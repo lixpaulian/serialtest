dea91da5b8c02b6d7e11d73dc8ab6127c6e70bbc
Lix N. Paulian <NPaulian@arri.at>
2017-07-26 17:15:08 +0200

    Added a command line interpreter and several initial commands.

--- serialtest/main.c
+++ serialtest/main.c
@@ -28,6 +28,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
@@ -38,13 +39,42 @@
 #include "frame-parser.h"
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 3
+#define VERSION_MINOR 4
 
 #define SERIAL_DEBUG 0
 
-static void quit (void);
-static int handle_serial_line (int fd);
+typedef enum
+{
+    GET_DUMP_STATE,
+    SET_DUMP_STATE
+} dump_request_t;
+
 static int counter = 0;
+
+static void
+quit (void);
+
+static int
+handle_serial_line (int fd, bool print);
+
+static bool
+dump_frames (dump_request_t operation, bool state);
+
+static int
+parse_line (char *line, size_t size);
+
+static int
+getver (int argc, char *argv[]);
+
+static int
+dump_rec (int argc, char *argv[]);
+
+static int
+quit_cmd (int argc, char *argv[]);
+
+static int
+help (int argc, char *argv[]);
+
 
 
 // Main entry point.
@@ -56,6 +86,7 @@ int main (int argc, char * argv[])
     int fd;
     struct termios options;
     int baudRate = 115200;
+    bool print = false;
     
     signal (SIGINT, (void *) quit);	/* trap ctrl-c calls here */
     
@@ -72,8 +103,7 @@ int main (int argc, char * argv[])
                 break;
                 
             case 'v':
-                fprintf (stdout, "Serial Test, version %d.%d, compiled on %s, %s\n",
-                        VERSION_MAJOR, VERSION_MINOR, __DATE__, __TIME__);
+                getver (0, NULL);
                 exit (EXIT_SUCCESS);
                 break;
                 
@@ -115,9 +145,9 @@ int main (int argc, char * argv[])
         fprintf (stdout, "Failed to open serial device %s\n", port);
         exit (EXIT_FAILURE);
     }
-
+    
     // prepare for multiple input sources
-    int res;
+    size_t res;
     fd_set readfs;
     struct timeval timeout;
     int maxfd = fd + 1;
@@ -146,13 +176,29 @@ int main (int argc, char * argv[])
         else if (FD_ISSET(fd, &readfs))
         {
             // got a frame from serial
-            res = handle_serial_line (fd);
+            res = handle_serial_line (fd, dump_frames (GET_DUMP_STATE, false));
         }
         else if (FD_ISSET(fileno (stdin), &readfs))
         {
-            // got a char from the user, just echo it back for now
-            putchar (getchar ());
-            res = 0;
+            // got a line from the user
+            char *line = NULL;
+            size_t size = 0;
+            
+            if ((res = getline (&line, &size, stdin)) > 0)
+            {
+                if (parse_line (line, res) < 0)
+                {
+                    // quit command
+                    res = 1;
+                }
+                else
+                {
+                    res = 0;
+                }
+                free (line);
+                fprintf (stdout, "> ");
+                fflush (stdout);
+            }
         }
     } while (res == 0);
     
@@ -163,7 +209,7 @@ int main (int argc, char * argv[])
 // @param   fd: serial file descriptor
 // @retval  0 if successful, 1 if serial port error.
 
-static int handle_serial_line (int fd)
+static int handle_serial_line (int fd, bool print)
 {
     ssize_t res;
     static uint8_t buff[400];
@@ -187,7 +233,10 @@ static int handle_serial_line (int fd)
         
         while ((result = parse_f0_f1_frames (&begin, &end)) == FRAME_OK)
         {
-            print_f0_f1_frames (begin, end - begin + 1);
+            if (print)
+            {
+                print_f0_f1_frames (begin, end - begin + 1);
+            }
             counter++;
             
             if (end < buff + res + offset) // whole buffer done?
@@ -209,6 +258,7 @@ static int handle_serial_line (int fd)
 #if SERIAL_DEBUG == 1
             fprintf (stdout, "-- truncated (len %ld)\n", end - begin + 1);
 #endif
+            // prepare to read more data from the serial port
             memmove (buff, begin, end - begin + 1);
             offset = end - begin + 1;
         }
@@ -224,6 +274,148 @@ static int handle_serial_line (int fd)
         return 1;
     }
 }
+
+static bool
+dump_frames (dump_request_t operation, bool state)
+{
+    static bool dump_frames_state = false;
+    
+    operation == SET_DUMP_STATE ? dump_frames_state = state : 0;
+    return dump_frames_state;
+}
+
+
+#define MAX_PARAMS 16
+#define OK 1
+#define ERROR 0
+#define QUIT -1
+
+typedef struct
+{
+    char *name;
+    int (*func) ();
+    char *help_string;
+} cmds_t;
+
+//===============================================================================
+// Commands table
+//
+const cmds_t rxcmds[] =
+//	CMD,	function,	  help string
+{
+    { "ver", getver, "Returns current version" },
+    { "dump", dump_rec, "Switch on/off dumping of received frames" },
+    { "quit", quit_cmd, "Quit program" },
+    { "exit", quit_cmd, "Exit program" },
+    { "help", help, "Show this help; for individual command help, use <command> -h" },
+    { NULL, NULL, 0 }
+} ;
+
+static int parse_line (char *line, size_t size)
+{
+    char *pbuff;
+    int result;
+    const cmds_t *pcmd;
+    char *argv[MAX_PARAMS]; /* pointers on parameters */
+    int argc; /* parameter counter */
+
+    pbuff = line;
+    *(pbuff + size - 1) = '\0'; // replace nl with null terminator
+    
+    while (*pbuff != ' ' && *pbuff != '\0')
+        pbuff++;
+    *pbuff++ = '\0'; // insert terminator
+    
+    result = ERROR; // init result just in case of failure
+    
+    /* iterate all commands in the table, one by one */
+    for (pcmd = rxcmds; pcmd->name; pcmd++) // lookup in the commands table
+    {
+        if (!strcasecmp (line, pcmd->name)) // valid command, parse parameters, if any
+        {
+            for (argc = 0; argc < MAX_PARAMS; argc++)
+            {
+                if (!*pbuff)
+                    break; 			// end of line reached
+                
+                if (*pbuff == '\"')
+                {
+                    pbuff++; 		// suck up the "
+                    argv[argc] = pbuff;
+                    while (*pbuff != '\"' && *pbuff != '\0')
+                        pbuff++;
+                    *pbuff++ = '\0';
+                    if (*pbuff)		// if not end of line...
+                        pbuff++;	// suck up the trailing space too
+                }
+                else
+                {
+                    argv[argc] = pbuff;
+                    while (*pbuff != ' ' && *pbuff != '\0')
+                        pbuff++;
+                    *pbuff++ = '\0';
+                }
+            }
+            result = (*pcmd->func) (argc, &argv[0]); // execute command
+            break;
+        }
+    }
+    return result;
+}
+
+//#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+static int
+getver (int argc, char *argv[])
+{
+    fprintf (stdout, "Serial Test Utility, version %d.%d, compiled on %s, %s\n",
+             VERSION_MAJOR, VERSION_MINOR, __DATE__, __TIME__);
+    return OK;
+}
+
+static int
+dump_rec (int argc, char *argv[])
+{
+    if (argc == 0)
+    {
+        fprintf (stdout, "Received frames are %sdumped to the console\n",
+                 dump_frames (GET_DUMP_STATE, false) ? "" : "not ");
+    }
+    else if (!strcasecmp (argv[0], "-h"))
+    {
+        fprintf (stdout, "Usage: dump { on | off }\n");
+    }
+    else if (!strcasecmp (argv[0], "on"))
+    {
+        dump_frames (SET_DUMP_STATE, true);
+    }
+    else if (!strcasecmp (argv[0], "off"))
+    {
+        dump_frames (SET_DUMP_STATE, false);
+    }
+    return OK;
+}
+
+static int
+quit_cmd (int argc, char *argv[])
+{
+    quit ();    // no return!
+    return OK;
+}
+
+static int
+help (int argc, char *argv[])
+{
+    const cmds_t *pcmd;
+    
+    fprintf (stdout, "Following commands are available:\r\n");
+    for (pcmd = rxcmds; pcmd->name; pcmd++)
+        fprintf (stdout, "  %-10s%s\r\n", pcmd->name, pcmd->help_string);
+    return OK;
+}
+
+
+//#pragma GCC diagnostic pop
 
 
 static void quit (void)
