4c8c1d558d8d8afb73c9cb027a0d10c294fc09f1
Lix N. Paulian <NPaulian@arri.at>
2017-07-31 11:13:16 +0200

    New file added: statistics. Functionality still to be implemented.

---
+++ serialtest/statistics.c
@@ -0,0 +1,53 @@
+//
+//  statistics.c
+//  serialtest
+//
+//  Copyright (c) 2017 Lix N. Paulian (lix@paulian.net)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+//  Created on 28/07/2017 (lnp)
+//
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "statistics.h"
+#include "frame-parser.h"
+
+
+void
+analyzer (uint8_t *data, size_t len)
+{
+    f0_f1_frame_t *frame = (f0_f1_frame_t *) data;
+    struct timespec tp;
+    
+    clock_gettime(CLOCK_MONOTONIC, &tp);
+    
+    switch (frame->header.type)
+    {
+        case LOW_LATENCY:
+            break;
+            
+        case FILE_XFER:
+            break;
+    }
+}
